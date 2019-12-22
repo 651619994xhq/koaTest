f64fc2f6e021d62fe30991c6982bae57d64fbfb9
xhq <651619994@qq.com>
2019-12-22 22:15:30 +0800

    提交代码

--- app/models/hot-book.js
+++ app/models/hot-book.js
@@ -39,9 +39,13 @@ class HotBook extends Model {
             group:['art_id'],     //分组
               //Sequelize.fn('COUNT') 对所有的分组art_id 进行求和      Sequelize.fn('SUM','score') //这个表示对所有的分数进行求和
             attributes:['art_id',[Sequelize.fn('COUNT',"*"),'count']]  //决定最后返回字段的名字
-        })
+        });
 
-        return favors
+        books.forEach(book=>{
+           HotBook._getEachBookStatus(book,favors);
+        });
+
+        return books
 
 
 
@@ -56,8 +60,16 @@ class HotBook extends Model {
         //nodejs  对cpu 要求低 充分榨取 单核的性能 利用率非常高 依赖硬件的成本很低 可以处理 资源密集 高并发 是可以快速切换 实现高并发
 
 
-
-
+    }
+    static _getEachBookStatus(book,favors){
+        let count = 0;
+        favors.forEach(favor =>{
+           if(book.id === favor.art_id){
+               count=favor.get('count');
+           }
+        });
+        book.setDataValue('count',count);
+        return book;
 
     }
 
